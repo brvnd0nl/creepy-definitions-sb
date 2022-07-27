49bafbfae215195ab9de1d2c2e673d4f12c15cac
Brandon Marchena <brandon.marchena@aplinet.co>
2022-07-27 12:20:49 -0500

    agregando hoja de estilo para separar los imports de las variables y clases globales

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import {SupabaseContextProvider} from '../context/SupabaseContext'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import '../styles/globalsImports.css';
 
 function MyApp({ Component, pageProps }) {
   return (
